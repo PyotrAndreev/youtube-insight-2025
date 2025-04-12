adb142ceb87fe00e183045b34bbb05a03781de20
arturshilnikov <shilnikov.ad@phystech.edu>
2025-04-12 10:06:08 +0300

    server without gpt

---
+++ core/profile_functions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8765eb2e045dd29f24a24925ed739daad91fbe7e7bf9bef598b786893429bdc
+size 3645
--- core/transcription/service.py
+++ core/transcription/service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47893aeadb59a1b835a5707ff781b14b873b6fc7e3cf986641137b9768646ac6
-size 2922
+oid sha256:a29ec41f78f7b8476ef9021e7b849e8fd578791b3fed99740670ce36bde26d22
+size 2923
