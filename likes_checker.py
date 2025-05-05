a7d99c0c1d7b3e86cf9a05b91e546b5c837cdccd
arturshilnikov <shilnikov.ad@phystech.edu>
2025-05-05 06:40:00 +0300

    add likes_checker

---
+++ likes_checker.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e7c3c38e5454b8bd96295f32c68b9f8c2b67816c6710b7b7f16745af47f976a
+size 2350
---
+++ tesе_likes.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:94bce4197b66aa3ce977ba7dcfec2b926a2fa57945596bbaefbc70c7499ef85b
+size 3507
